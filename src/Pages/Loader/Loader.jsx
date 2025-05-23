9b7c83dbb45e85e14108a45c160614d7e4b3dbe3
mbdullah <mdabdullah162005@gmail.com>
2025-05-23 13:08:48 +0600

    included react-lottie loading spiner in loader page

--- src/Pages/Loader/Loader.jsx
+++ src/Pages/Loader/Loader.jsx
@@ -1,7 +1,15 @@
+import Lottie from "lottie-react";
+import loadingAnimation from "../../assets/Animation/spiner.json"; // adjust path if needed
+
 const Loader = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <span className="loading loading-spinner loading-xl text-primary"></span>
+     <div className="min-h-screen flex justify-center items-center bg-white dark:bg-gray-900">
+      <Lottie
+        animationData={loadingAnimation}
+        loop
+        autoplay
+        style={{ height: 200, width: 200 }}
+      />
     </div>
   );
 };
